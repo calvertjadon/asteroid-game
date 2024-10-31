825713ac71350b516eb8c38f0d460e65d03477f0
Jadon Calvert <calvertjadon@gmail.com>
2024-10-31 16:30:42 -0400

    add player

---
+++ circleshape.py
@@ -0,0 +1,22 @@
+import pygame
+from abc import ABC, abstractmethod
+
+
+# Base class for game objects
+class CircleShape(pygame.sprite.Sprite, ABC):
+    def __init__(self, x, y, radius):
+        # we will be using this later
+        if hasattr(self, "containers"):
+            super().__init__(self.containers)  # type: ignore
+        else:
+            super().__init__()
+
+        self.position = pygame.Vector2(x, y)
+        self.velocity = pygame.Vector2(0, 0)
+        self.radius = radius
+
+    @abstractmethod
+    def draw(self, screen) -> None: ...
+
+    @abstractmethod
+    def update(self, dt) -> None: ...
--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from constants import (
     ASTEROID_SPAWN_RATE,
     ASTEROID_MAX_RADIUS,
 )
+from player import Player
 
 
 def main():
@@ -17,13 +18,21 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         screen.fill((0, 0, 0))
+        player.draw(screen)
+
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
